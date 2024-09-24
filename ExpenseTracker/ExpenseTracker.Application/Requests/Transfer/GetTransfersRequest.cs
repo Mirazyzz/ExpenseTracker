14a44473038763d20dd8fd963849233f8d836e49
Jamshidbek <161879703+jamshidbekbahtiyorivich@users.noreply.github.com>
2024-09-24 12:47:29 +0500

    requests added

---
+++ ExpenseTracker/ExpenseTracker.Application/Requests/Transfer/GetTransfersRequest.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseTracker.Application.Requests.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Application.Requests.Transfer
+{
+    public class GetTransfersRequest:UserRequest
+    {
+        public string? Search {  get; set; }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker.Application/Requests/Transfer/UpdateTransferRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Application.Requests.Transfer
+{
+    public class UpdateTransferRequest:CreateTransferRequest
+    {
+        public int TransferId { get; set; } 
+    }
+}
