54ea4e9b7d913a87131133eff4d5cb5d0f88d0c3
Abdulazim Makhmudjanov <159573274+Abdulazim1771@users.noreply.github.com>
2024-10-18 14:01:42 +0500

    Update site.js

--- ExpenseTracker/ExpenseTracker/wwwroot/js/site.js
+++ ExpenseTracker/ExpenseTracker/wwwroot/js/site.js
@@ -1,17 +1,1 @@
-﻿document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.e-menu-item').forEach(link => {
-        const linkText = link.textContent.toLowerCase();
-        const currentPath = location.pathname.toLowerCase();
-        if (linkText === 'dashboard' && currentPath === '/') {
-            link.classList.add('active-menu-item');
-            console.log('Dashboard active:', link);
-        }
-        else if (currentPath.endsWith(linkText)) {
-            link.classList.add('active-menu-item');
-            console.log(link);
-        }
-        else {
-            link.classList.remove('active-menu-item');
-        }
-    });
-});
+
